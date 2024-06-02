085b3f429f1c74b7748100d0f4b9e2f0036d96de
Nayan <nayanbebale2003@gmail.com>
2024-06-02 15:50:20 +0530

    somechnage

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI", 'sqlite:///portfolio.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
